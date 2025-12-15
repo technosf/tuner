b8e46c701523abb31d86fa1e8777c86bb8adc6e6
technosf <git config --global user.email>
2025-12-15 13:08:27 -0800

    Updated pot file for translation

--- meson_clean.sh
+++ meson_clean.sh
@@ -1,5 +1,6 @@
 rm -r builddir
-meson setup --buildtype=debug builddir
-meson compile -C builddir
+#meson setup builddir -Dtranslation=update
+meson setup --buildtype=debug builddir -Dtranslation=update
+meson compile -C builddir pot
 #flatpak-builder --force-clean --user --sandbox --install build-dir com.github.louis77.tuner.yml
 
